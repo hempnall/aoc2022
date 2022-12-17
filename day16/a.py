f7b1b1bc3d81bc9603a2c39574f8b6b79306d23c
James Hook <jameshook@Jamess-MBP.broadband>
2022-12-17 18:46:57 +0000

    done day 16

--- day16/a.py
+++ day16/a.py
@@ -1,4 +1,5 @@
-is_real=False
+
+is_real=True
 
 f=open("sample.txt","r")
 lines=[ l.strip() for l in f.readlines() ]
@@ -101,35 +102,150 @@ graph_nodes=construct_graph(lines)
 # #render_graph(graph_nodes)
 simplified_graph=simplify_graph(graph_nodes)
 #render_simple_graph(simplified_graph)
-time_limit=30
+time_limit=26
 current_max=0
+max_time=0
 
-def maximise_rate(node_st,current_total,increase,current_time):
-    global current_max
-    #print(f'[{current_time}] {node_st} {current_total} {on_taps}')
-    if current_time>time_limit:
-        #print(f'{node_st} {current_total}')
-        return current_total
+def calculate_tap_score(taps):
+    tap_score=0
+    for k,v in taps.items():
+        tap_score += (time_limit - v["on_time"] ) * v["rate"]
+    return tap_score
 
-    current_total += increase
-    current_max=max(current_max,current_total)
-    next_node=node_st[-1]
-    current_node=simplified_graph[next_node]
+def show_tap_scores(taps):
+    in_ord=dict(sorted(taps.items(), key=lambda item: item[1]["on_time"]))
+    for k,v in in_ord.items():
+        print(f'{k}: on_time={v["on_time"]} rate={v["rate"]} by={v["by"]} total={(time_limit - v["on_time"] ) * v["rate"]}')
+    print(calculate_tap_score(taps))
+    print() 
 
-    for k,v in current_node.items():
-        if k in node_st:
+
+def compare_stack(stack2,stack1):
+    return stack1 == stack2[:len(stack1)]
+
+def sum_maxs(mp):
+    return sum(mp.values())
+
+def maximise_rate(
+    me_stack,
+    el_stack,
+    you_remain, # in transit remaining distance for me
+    el_remain, # in transit remaining for ele
+    current_time,
+    tap_scores,
+    score_maxs):   # current time
+
+    global current_max, max_time
+    DEBUG=False
+    assert(el_remain>=0)
+    assert(you_remain>=0)
+    me_dest=me_stack[-1]
+    el_dest=el_stack[-1]
+    max_time=max(current_time,max_time)
+
+    if current_time > time_limit:
+        return
+
+    total=calculate_tap_score(tap_scores)
+    if total > current_max:
+        current_max=max(current_max,total)
+
+    if total + ((time_limit-current_time - 2) * sum_maxs(score_maxs)) <= current_max:
+        return
+
+    def get_effective_node(remain,node):
+        next_node=node
+        if remain > 0:
+            return True , { next_node: remain }
+        else:
+            return False, simplified_graph[next_node]
+
+    def new_stack( cur_stack , is_virtual, new_dest ):
+        if is_virtual:
+            return cur_stack
+        else:
+            return [*cur_stack,new_dest]
+
+    el_virtual, el_next_node = get_effective_node(el_remain,el_dest)
+    me_virtual, me_next_node = get_effective_node(you_remain,me_dest)
+
+    for k_me,v_me in me_next_node.items():
+        if k_me == "AA":
             continue
-        next_time=current_time+v+1
-        rate=graph_nodes[k]["rate"]
-        increase=(time_limit-next_time)*rate
-        maximise_rate(
-            [*node_st,k],
-            current_total,
-            increase,
-            next_time)
+        if not me_virtual and k_me in tap_scores:
+            continue
 
-max_steam=maximise_rate(["AA"],0,0,0)
+        for k_ele,v_ele in el_next_node.items():
+            if k_ele == "AA":
+                continue
+            if k_me == k_ele:
+                continue
+            if not el_virtual and k_ele in tap_scores:
+                continue
+
+            new_ele_stack = new_stack(el_stack,el_virtual,k_ele)
+            new_me_stack = new_stack(me_stack,me_virtual,k_me)
+
+            next_tap_scores=tap_scores.copy()
+            new_maxes=score_maxs.copy()
+
+            if not el_virtual:
+                del new_maxes[k_ele]
+                next_tap_scores[k_ele]={
+                    "on_time": current_time+v_ele+1,
+                    "rate": graph_nodes[k_ele]["rate"],
+                    "by": "ele"
+                }
+
+            if not me_virtual:  
+                del new_maxes[k_me]
+                next_tap_scores[k_me]={
+                    "on_time": current_time+v_me+1,
+                    "rate": graph_nodes[k_me]["rate"],
+                    "by": "me"
+                }
+
+
+            time_delta=0
+            el_rm=0
+            me_rm=0
+
+            #print(f'you_remain={you_remain} el_remain={el_remain} v_e={v_ele} v_m={v_me}')
+            if you_remain == 0 and el_remain == 0:
+                time_delta=min(v_me,v_ele)+1
+                el_rm=1 + v_ele-time_delta
+                me_rm=1 + v_me-time_delta
+            elif you_remain==0 and el_remain >0:
+                time_delta=min(v_me+1,v_ele)
+                el_rm=v_ele-time_delta
+                me_rm=1 + v_me-time_delta
+            elif you_remain>0 and el_remain == 0:
+                time_delta=min(v_me,v_ele+1)
+                el_rm=1 + v_ele-time_delta
+                me_rm=v_me-time_delta
+            else:
+                raise Exception("impossible")
+
+            next_time=current_time + time_delta
+
+            maximise_rate(
+                new_me_stack,
+                new_ele_stack,
+                me_rm,
+                el_rm,
+                next_time,
+                next_tap_scores,
+                new_maxes
+            )
+
+tap_scores={}
+max_tap_scores = {
+    k:graph_nodes[k]["rate"] for k in graph_nodes.keys()
+}
+max_steam=maximise_rate(["AA"],["AA"],0,0,0,tap_scores,max_tap_scores)
+print(tap_scores)
 print(current_max)
+print(max_time)
 
 
 
