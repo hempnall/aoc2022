047f3843646f45e565cd45bc9b5b81e0b81d2c8f
James Hook <jameshook@Jamess-MBP.broadband>
2022-12-23 08:28:17 +0000

    wip2

--- day23/a.py
+++ day23/a.py
@@ -6,8 +6,14 @@ DIRS={
     "NE": (1,-1),
     "E": (1,0),
     "SE": (1,1),
-    "S": 
+    "S": (0,1),
+    "SW": (-1,1),
+    "W": (-1,0),
+    "NW": (-1,-1)
 }
+
+def addvec(vec1,vec2):
+    return (vec1[0]+vec2[0], vec1[1]+vec2[1])
 
 def parse_file(lines):
     elves=set()
@@ -18,9 +24,26 @@ def parse_file(lines):
         elves.update(elves_in_line)
     return elves
 
+def has_adjacent_elf(  elves, elf  ):
+    for DIR in DIRS.values():
+        adj_pos = addvec(elf,DIR)
+        if adj_pos in elves:
+            return True
+    return False
+        
+def elves_bounding_box_area(elves):
+    minx=min([ x for x,_ in elves])
+    miny=min([ y for _,y in elves])
+    maxx=max([ x for x,_ in elves])
+    maxy=max([ y for _,y in elves])
+    return (maxx-minx+1) * (maxy - miny+1)
+
+
+
 elves=parse_file(lines)
 print(elves)
-
+boundingbox_area=elves_bounding_box_area(elves)
+print(boundingbox_area)
 
 
 
